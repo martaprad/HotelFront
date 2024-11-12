9d1ba0995373230042d249d48045c16c3e75bf49
Javier Gonzalez <javier.gonzalez@techedgegroup.com>
2024-11-12 22:32:36 +0100

    formulario reservas con transfer y pago

--- js/reserva.js
+++ js/reserva.js
@@ -1,88 +1,102 @@
-document.getElementById('reservas-form').addEventListener('submit', function(event) {
+let token = localStorage.getItem('token');
+
+document.getElementById('reservas-form').addEventListener('submit', function (event) {
     event.preventDefault();  // Prevenir el envío del formulario
 
     let fInicio = document.getElementById('fechaInicio').value;
     let fFin = document.getElementById('fechaFin').value;
     let habTipoId = document.getElementById('tipoHab').value;
     let transf = document.getElementById('transfer').value;
-    let actividades = document.getElementById('actividades');
-    //let tipoActividad = [];
-    //let listActividades = {tipoActividad : []};
+    let actividadesList = document.getElementById('actividades');
 
-    if (habTipoId ==="1" || habTipoId ==="2" ||  habTipoId ==="3" ){
-        
-        /*Array.from(actividades.options).forEach(function (option) {
 
-            if (option.selected === true) {
-                
-                tipoActividad.push(option.value);
-                
-                
-            };
-        });
-        //listActividades = { tipoActividad: listActividades }; 
+    //Crear el Json para enviar en el body del fetch
+    var myJson = {
+        fechaInicio: fInicio,
+        fechaFin: fFin,
+        habitacionTipoId: habTipoId,
+        actividadDTOList: [],
+        trasladoDTO: {
+            tipoTraslado: transf
+        }
+    };
 
-        transf = {tipoTraslado: transf}; */
+    //Recorrer las actividades seleccionadas 
+    for (let i = 0; i < actividadesList.selectedOptions.length; i++) {
 
-        alert(fInicio + " " + fFin + " " + habTipoId +" " + actividades + " " + transf);
-        
-    } else {
+        myJson.actividadDTOList.push({ tipoActividad: actividadesList.selectedOptions[i].value });
 
-        alert("Tienes que seleccionar una habitación");
     }
 
-
-    let token = localStorage.getItem('token');
-    console.log(token);
-
     // Hacer la llamada a la API de reservas
-    
-    fetch('http://localhost:8080/reservas', {  
+    fetch('http://localhost:8080/reservas', {
         method: 'POST',
-        //mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
         },
-        body: JSON.stringify({
-            fechaInicio: fInicio,
-            fechaFin: fFin,
-            habitacionTipoId: habTipoId,
-            actividadDTOList: [
-                { 
-                    tipoActividad : "SURF"
-                },
-                {
-                    tipoActividad : "SPA"
-                }
-        
-            ],
-            trasladoDTO: {
-                tipoTraslado : "IDAVUELTA"
+        body: JSON.stringify(myJson)
+    })
+
+        .then(response => {
+            if (response.ok) {
+                // La respuesta es exitosa (código 2xx)
+                return response.text();
+            } else {
+                // La respuesta tiene un código de error
+                return response.text().then(errorMessage => { throw new Error(errorMessage); });
             }
-            })
-        
-    })
-    .then(response => {
-        if (response.ok) {
-            // La respuesta es exitosa (código 2xx)
-            return response.text();
-        } else {
-            // La respuesta tiene un código de error
-            return response.text().then(errorMessage => { throw new Error(errorMessage); });
-        }
-    })
-    .then(data => {
-        // Manejar respuesta exitosa
-        console.log(data);
-        alert(data);
-        return data;
-     })
-    .catch(error => {
-        // Manejar error
-        console.error('Error:', error.message);
-        alert('Error: ' + error.message);
-    });
-    
- });
+        })
+
+        .then(data => {
+            // Manejar respuesta exitosa
+            //alert("Ha reservado una habitacion " + data);
+
+                // Hacer la llamada a la API calcularPrecio
+                fetch('http://localhost:8080/reservas/calcularPrecio', {
+                    method: 'POST',
+                    //mode: 'no-cors',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify(myJson)
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            // La respuesta es exitosa (código 2xx)
+                            return response.text();
+                        } else {
+                            // La respuesta tiene un código de error
+                            return response.text().then(errorMessage => { throw new Error(errorMessage); });
+                        }
+                    })
+                    .then(data => {
+
+                        // Manejar respuesta exitosa
+                        if (habTipoId == "1") {
+                            alert("Ha reservado una habitación Double desde " + fInicio + " hasta " + fFin + " el precio total es " + data);
+                        } else if (habTipoId == "2") {
+                            alert("Ha reservado una habitación Triple desde " + fInicio + " hasta " + fFin + " el precio total es " + data);
+                        } else {
+                            alert("Ha reservado una habitación Deluxe desde " + fInicio + " hasta " + fFin + " el precio total es " + data);
+                        }
+                        return data;
+                    })
+                    .catch(error => {
+                        // Manejar error
+                        console.error('Error:', error.message);
+                        alert('Error: ' + error.message);
+                    });
+
+            return data;
+        })
+
+        .catch(error => {
+            // Manejar error
+            alert('Error: ' + error.message);
+        });
+
+});
+
 
